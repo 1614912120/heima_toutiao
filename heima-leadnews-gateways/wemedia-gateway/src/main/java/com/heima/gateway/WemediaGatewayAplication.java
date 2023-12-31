257e8f7c72eff0225fdb4748cedecb304eaba10f
RJRen <1614912120@qq.com>
2023-12-31 11:18:15 +0800

    黑马点评 day03

---
+++ heima-leadnews-gateways/wemedia-gateway/src/main/java/com/heima/gateway/WemediaGatewayAplication.java
@@ -0,0 +1,11 @@
+package com.heima.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WemediaGatewayAplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WemediaGatewayAplication.class, args);
+    }
+}
